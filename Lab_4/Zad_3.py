837a0124ad20bf4bda9b27f056cec452ece7aa03
beben <bebenprzemek03@gmail.com>
2023-04-13 15:06:27 +0200

    po labo 5

--- Lab_4/Zad_3.py
+++ Lab_4/Zad_3.py
@@ -1,18 +1,18 @@
 def najwieksza(a, start, koniec):
     if start == koniec:
         maximum = a[start]
-        return (maximum)
+        return maximum
     elif start + 1 == koniec:
         if a[start] > a[koniec]:
             maximum = a[start]
         else:
             maximum = a[koniec]
-        return (maximum)
+        return maximum
     else:
         srodek = int((start + (koniec - start) / 2))
         max_l = najwieksza(a, start, srodek)
         max_p = najwieksza(a, srodek + 1, koniec)
-    return (max(max_l, max_p))
+    return max(max_l, max_p)
 
 a = [1,8,19,20,35,14,7]
 koniec = len(a)-1
